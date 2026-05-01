99ad40beeec15eed3809cf99d1c80838ec47c0d3
Shivam Kumar <shivamlpu1214@gmail.com>
2026-05-01 13:05:58 +0530

    loading added6

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,10 +1,11 @@
 import Lottie from "lottie-react";
+import animationData from "../assets/loader.json";
 
 function Loader() {
   return (
     <div className="flex flex-col items-center justify-center mt-16">
       <div className="w-32 h-32">
-        <Lottie path="/loader.json" loop={true} />
+        <Lottie animationData={animationData} loop={true} />
       </div>
 
       <p className="text-slate-400 mt-4 animate-pulse">
