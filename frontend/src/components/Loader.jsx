48b448c65336bb86245a86e483c0551a49c7de1f
Shivam Kumar <shivamlpu1214@gmail.com>
2026-05-01 13:02:12 +0530

    loading added4

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,17 @@
+import Lottie from "lottie-react";
+
+function Loader() {
+  return (
+    <div className="flex flex-col items-center justify-center mt-16">
+      <div className="w-32 h-32">
+        <Lottie path="/loader.json" loop={true} />
+      </div>
+
+      <p className="text-slate-400 mt-4 animate-pulse">
+        Loading requests...
+      </p>
+    </div>
+  );
+}
+
+export default Loader;
