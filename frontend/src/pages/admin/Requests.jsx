7bb11256d51d1ab15b3da79c4f0fa6eae7e398ab
Shivam Kumar <shivamlpu1214@gmail.com>
2026-05-01 13:03:34 +0530

    loading added5

--- frontend/src/pages/admin/Requests.jsx
+++ frontend/src/pages/admin/Requests.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Loader from "../components/Loader";
+import Loader from "../../components/Loader";
 
 function Requests() {
   const [requests, setRequests] = useState([]);
