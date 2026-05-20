315be3b4bd48c02e03ec7befa09173a56ca77775
Shivam Kumar <shivamlpu1214@gmail.com>
2026-05-20 23:35:00 +0530

    request updated

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,21 +1,10 @@
-// function Loader() {
-//   return (
-//     <div className="flex flex-col items-center mt-16">
-//       <div className="w-10 h-10 border-4 border-slate-300 border-t-slate-900 rounded-full animate-spin"></div>
-//       <p className="mt-3 text-slate-500">Loading requests...</p>
-//     </div>
-//   );
-// }
-
-// export default Loader;
-
 function Loader() {
   return (
     <div className="flex flex-col items-center mt-16">
       <img
         src="/loader.gif"
         alt="loading"
-        className="w-20 h-20"
+        className="w-50 h-50"
       />
       <p className="mt-3 text-slate-500">Loading requests...</p>
     </div>
