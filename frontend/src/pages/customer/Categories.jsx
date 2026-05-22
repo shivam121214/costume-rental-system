09b8f7b1cea1ce97e4f05764fc0227f7d5935b6c
Shivam Kumar <shivamlpu1214@gmail.com>
2026-05-22 10:40:23 +0530

    minor changes

--- frontend/src/pages/customer/Categories.jsx
+++ frontend/src/pages/customer/Categories.jsx
@@ -89,7 +89,7 @@ export function Categories() {
               Pick Your Adventure!
             </motion.h2>
           </div>
-          <Link to="/products" className="block">
+          <Link to="/themes" className="block">
             <button className="bg-white text-black px-6 py-3 rounded-xl border-2 border-black shadow-[4px_4px_0_0_rgba(0,0,0,1)] hover:shadow-[2px_2px_0_0_rgba(0,0,0,1)] hover:translate-x-1 hover:translate-y-1 font-bold transition-all text-lg">
               See All Themes
             </button>
